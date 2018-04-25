ebd47b5802bff3c32360b13805621999e8bd5283
qicosmos <383121719@qq.com>
2018-04-25 09:02:52 +0800

    Update inja.hpp

--- inja.hpp
+++ inja.hpp
@@ -32,12 +32,6 @@ SOFTWARE.
 #define PANTOR_INJA_VERSION_MINOR 0
 #define PANTOR_INJA_VERSION_PATCH 1
 
-
-#ifndef NLOHMANN_JSON_HPP
-	static_assert(false, "nlohmann/json not found.");
-#endif
-
-
 #include <string>
 #include <sstream>
 #include <fstream>
@@ -47,7 +41,7 @@ SOFTWARE.
 #include <vector>
 #include <algorithm>
 #include <type_traits>
-
+#include "json.hpp"
 
 namespace inja {
 
